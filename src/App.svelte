4f805800b4b3356c37ced6c3bbbc5ade22b4d40f
gretmn102 <erlgreywatson@gmail.com>
2025-05-05 23:42:44 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { concat } from "./utils"
+</script>
+
+<main>
+  <div class={concat([
+    "flex",
+    "justify-center",
+  ])}>
+    <div class={concat([
+      "relative", // for children with absolute
+      "w-full",
+      "max-w-[480px]",
+      "h-screen",
+      "bg-[#e5e5e5]",
+      "flex",
+      "flex-col",
+    ])}>
+      Hello world!
+    </div>
+  </div>
+</main>
